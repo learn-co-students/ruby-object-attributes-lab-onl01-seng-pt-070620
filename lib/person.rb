1a74a630738ab41745cd9323a29f6b1f39e5741f
Katie Le <lethikatie@gmail.com>
2020-07-20 21:17:04 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+  
+  def name=(persons_name)
+    @name = "Beyonce"
+  end 
+
+  def name 
+    @name
+  end 
+
+ def job=(persons_job)
+    @job = "Singer"
+  end 
+
+  def job
+    @job 
+  end 
+end 
+
