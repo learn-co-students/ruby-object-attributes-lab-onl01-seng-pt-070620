a194b0b6e444dac08d013ea0400ca334d0139cc2
Jill Short <jillian.b.short@gmail.com>
2020-07-21 19:26:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,25 @@ class Dog
     @name = dogs_name
   end
   
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+  
   def name
     @name
   end
+  
+  def breed
+    @breed
 end
+
+  
 
 fido = Dog.new 
 fido.name = "Fido"
 
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+
 puts fido.name
+puts snoopy.breed
