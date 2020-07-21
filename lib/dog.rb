6f8b9a6f36f9a9be7d2a62473907f87fe82d9419
Katie Le <lethikatie@gmail.com>
2020-07-21 21:10:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,17 @@
 class Dog 
-  def breed=(dog_breed)
-    @breed = "Beagle"
-  end 
-  
-  def breed 
-    @breed
-  end 
-  
   def name=(dogs_name)
     @name = "Fido"
 end 
 
-  def name
-    @name
+  def name 
+    @name 
+  end 
+
+  def breed=(dogs_breed)
+    @breed = "Beagle"
+  end 
+  
+  def breed
+    @breed
   end 
 end 
-  
-fido = Dog.new
-fido.name = "Fido"
- 
-puts fido.name
-
-beagle = Dog.new
-beagle.breed = "Beagle"
-
-puts beagle.breed
