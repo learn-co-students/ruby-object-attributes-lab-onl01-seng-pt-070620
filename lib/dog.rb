420cfa843962198a389b4ef06f711cde942fd68f
Lauren Stone <lstone321@gmail.com>
2020-07-21 04:40:04 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog 
+  
+  def dog_name(name)
+    @dog_name = name
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def name=(doggy_name) 
+    @name = doggy_name
+  end 
+  
+  def dog_breed(breed)
+    @breed = breed 
+  end 
+  
+  def breed 
+    @breed 
+  end 
+  
+  def breed=(breed_type)
+    @breed = breed_type
+  end 
+end 
