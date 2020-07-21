3a6413269574456b3bd9ee5ad383c759c792de96
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-21 00:26:20 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  
+  def name= name
+    @name= name
+  end
+  
+  def name
+    @name
+  end
+end
+  
+  class Person
+    
+  def job= persons_job
+    @job= persons_job
+  end
+  
+  def job
+    @job
+  end
+end
+
+  
