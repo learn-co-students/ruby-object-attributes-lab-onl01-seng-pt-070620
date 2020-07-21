68c55670596384b5fc789b12ab87cc2c125702dd
Lauren Stone <lstone321@gmail.com>
2020-07-21 05:12:41 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+
+ def name=(person_name)
+  @name = person_name
+ end
+ 
+ def their_name(name)
+   @their_name = name
+ end 
+ 
+ def name
+   @name
+ end
+ 
+ def job=(job_name)
+   @job = job_name
+ end 
+ 
+ def job_title(job)
+   @job_title = job 
+ end 
+ 
+ def job 
+   @job 
+ end 
+end 
